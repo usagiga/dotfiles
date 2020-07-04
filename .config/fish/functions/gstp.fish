f6a9a1365c3be150a8457f23085b5f545df60a6f
usagiga <usagigawara@gmail.com>
2020-07-04 15:48:21 +0900

    Revise gstp alias to get all staging file free

--- .config/fish/functions/gstp.fish
+++ .config/fish/functions/gstp.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gstp --wraps='git stash pop' --description 'alias gstp git stash pop'
-  git stash pop $argv;
+function gstp --wraps='git stash pop' --wraps='git stash pop; git restore --staged *' --description 'alias gstp git stash pop; git restore --staged *'
+  git stash pop; git restore --staged * $argv;
 end
