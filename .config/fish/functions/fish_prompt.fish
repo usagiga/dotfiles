249a0f036625943e2060d5c59e0dc690c9c5bcf5
usagiga <usagigawara@gmail.com>
2020-12-03 15:51:15 +0900

    Change prompt more slim

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,7 +5,7 @@ function fish_prompt --description 'Write out the prompt'
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
     set -l prefix
-    set -l suffix '>'
+    set -l suffix '$'
     if contains -- $USER root toor
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
@@ -22,5 +22,5 @@ function fish_prompt --description 'Write out the prompt'
     # Write pipestatus
     set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
+    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
 end
