8e17439c5e1fb4fb4c02040cb27358f089d437e3
usagiga <usagiga@usagiga.me>
2021-02-15 23:03:42 +0900

    Add GPG key settings for git

--- .config/fish/functions/gc.fish
+++ .config/fish/functions/gc.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gc --description 'alias gc git commit -m'
-	git commit -m $argv;
+function gc --description 'alias gc git commit -S -m'
+	git commit -S -m $argv;
 end
