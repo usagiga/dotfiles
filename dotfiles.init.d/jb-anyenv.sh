0e67772cf285d346b1b457b79e84fd4ae2b7f177
usagiga <usagiga@usagiga.me>
2021-07-10 18:50:18 +0900

    feat: enable **env from anyenv for JetBrains IDE

---
+++ dotfiles.init.d/jb-anyenv.sh
@@ -0,0 +1,5 @@
+echo "Enable **env from anyenv in Jetbrains IDE"
+
+ln -s ${HOME}/.anyenv/envs/rbenv ${HOME}/.rbenv
+ln -s ${HOME}/.anyenv/envs/nodenv ${HOME}/.nodenv
+ln -s ${HOME}/.anyenv/envs/plenv ${HOME}/.plenv
