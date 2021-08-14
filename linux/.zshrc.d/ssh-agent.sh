bd341e98ea4d54cba96d7c6cba8f0f622302219d
usagiga <usagiga@usagiga.me>
2021-08-14 11:26:19 +0900

    feat: enable ssh-agent

--- linux/.zshrc.d/ssh-agent.sh
+++ linux/.zshrc.d/ssh-agent.sh
@@ -1,7 +1,7 @@
 ### For linux
-# if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-#     ssh-agent > '/tmp/ssh-agent.env'
-# fi
-# if [[ ! "$SSH_AUTH_SOCK" ]]; then
-#     source '/tmp/ssh-agent.env' > /dev/null
-# fi
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > '/tmp/ssh-agent.env'
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    source '/tmp/ssh-agent.env' > /dev/null
+fi
