ba3007bb27e4b856de4223ae71dd8b08172d8da1
usagiga <usagiga@usagiga.me>
2022-07-01 08:35:18 +0900

    fix: workaround for perl / gcloud

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,11 @@ if status is-interactive
         exec tmux
     end
 end
+
+
+# Work around for perl
+set -x PATH "$HOME/.anyenv/envs/plenv/bin:$PATH"
+set -x PATH "$HOME/.anyenv/envs/plenv/shims:$PATH"
+
+# Work around for gcloud
+set -x CLOUDSDK_PYTHON 'python2.7' 
