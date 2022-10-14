8bf53f6c9eb50c35d89e595ad497f5222882e7b2
usagiga <usagiga@usagiga.me>
2022-10-14 19:11:47 +0900

    fix: remove --allow-empty from gc to reduce mistakes

--- .config/fish/functions/gc.fish
+++ .config/fish/functions/gc.fish
@@ -1,3 +1,3 @@
 function gc --wraps='git commit -m' --wraps='git commit --allow-empty -m' --description 'alias gc git commit --allow-empty -m'
-  git commit --allow-empty -m $argv; 
+  git commit -m $argv; 
 end
