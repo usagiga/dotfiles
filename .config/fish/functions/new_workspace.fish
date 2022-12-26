224d5e375c91d9722b824c1b64af66b5fb25ed11
usagiga <usagiga@usagiga.me>
2022-12-27 04:34:14 +0900

    feat: add new_workspace alias

---
+++ .config/fish/functions/new_workspace.fish
@@ -0,0 +1,12 @@
+function new_workspace
+  set WORKSPACE_DIR "$(ghq root)/workspace/$argv"
+  if [ -d $WORKSPACE_DIR ]
+    echo 'directory already exists' 1>&2
+    exit 1
+  end
+
+  mkdir -pv $WORKSPACE_DIR
+  cd $WORKSPACE_DIR
+
+  git init -b master
+end
