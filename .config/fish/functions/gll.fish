7ea45d7e79302d6ff8db3ab431917ec63be0e05d
usagiga <usagiga@usagiga.me>
2024-07-01 10:47:05 +0900

    feat: define gll alias for git log with fzf

--- .config/fish/functions/gll.fish
+++ .config/fish/functions/gll.fish
@@ -1,3 +1,11 @@
-function gll --wraps='gl -n 5' --description 'alias gll gl -n 5'
-  gl -p $argv; 
+function gll --wraps='git log | fzf' --description 'git log with fizzy finding'
+    set -l branchname (
+        env FZF_DEFAULT_COMMAND='git --no-pager branch -a | grep -v HEAD | sed -e "s/^.* //g"' \
+            fzf --height 70% --prompt "BRANCH NAME>" \
+            --preview "git --no-pager log -20 --color=always {}"
+    )
+
+    if test -n "$branchname"
+        git checkout (echo "$branchname"| sed "s#remotes/[^/]*/##")
+    end
 end
